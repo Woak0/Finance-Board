799d057a6877f09ede8ad031f6a2a945b7873188
Woak0 <malekelbenni@gmail.com>
2025-06-08 03:38:02 +1000

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -35,17 +35,50 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            pass
-            print("\n-> Add Debt")
+            print("\n--- Add New Debt ---")
+            debt_label = input("Enter debt name: ")
+
+            while True:
+                amount_str = input("Enter the amount: ")
+                try:
+                    amount = float(amount_str)
+                    break
+                except ValueError:
+                    print("Invalid amount. Please enter a valid number (e.g., 50.75).")
+            print(f"You entered a valid amount: {amount}")
+            Debt_Manager.add_debt(label=debt_label, amount=amount)
+
         elif choice == '2':
             pass
             print("\n-> Make Payment")
+            
         elif choice == '3':
             pass
             print("\n-> Show Summary")
+
         elif choice == '4':
-            pass
-            print("\n-> List all Entries")
+            print("\n--- Listing all Debt Entries ---")
+            all_debts = Debt_Manager.get_all_debts()
+
+            if not all_debts:
+                print("No debts recorded.")
+            else:
+                for debt in all_debts:
+                    short_debt_id = debt.id[:8]
+                    date_str = debt.date_incurred.strftime("%Y-%m-%d")
+                    print(f"ID: {short_debt_id} | Date: {date_str} | label: {debt.label} | Amount: ${debt.amount:.2f}")
+
+            print("\n--- Listing all Payment Entries---")
+            all_payments = Payment_Manager.get_all_payments()
+
+            if not all_payments:
+                print("No payments recorded.")
+            else:
+                for payment in all_payments:
+                    short_debt_id_for_payment = payment.debt_id[:8]
+                    date_str_payment = payment.date_paid.strftime("%Y-%m-%d")
+                    print(f"  Date: {date_str_payment} | Paid ${payment.amount:7.2f} towards Debt ID {short_debt_id_for_payment} (Label: {payment.label})")
+
         elif choice.lower() == 'q':
             print("Quitting and Saving application...")
             break
@@ -59,9 +92,6 @@ def main():
         new_debt = Debt_Manager.add_debt(label = "Coffee",amount = 5)
 
         new_payment = Payment_Manager.add_payment(debt_id=new_debt.id, amount = 3)
-
-        all_debts = Debt_Manager.get_all_debts()
-        all_payments = Payment_Manager.get_all_payments()
 
         total_debt_incurred = calculate_total_debt_incurred(all_debts)
         total_amount_paid = calculate_total_amount_paid(all_payments)
